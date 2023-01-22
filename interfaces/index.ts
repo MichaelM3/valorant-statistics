586f29ec467cc00a253c3b86fa66efcc458cfea3
michaelm3 <michael.muniz94@gmail.com>
2023-01-22 11:48:41 -0500

    added lib,interface,env folders. completed fetch function and act interface

---
+++ interfaces/index.ts
@@ -0,0 +1,7 @@
+export interface Act {
+    id: string;
+    parentId: string;
+    type: string;
+    name: string;
+    isActive: string;
+}
---
+++ lib/fetchValContent.ts
@@ -0,0 +1,15 @@
+import { Act } from "../interfaces"
+
+
+export const fetchValContent = async (): Promise<Act[]> => {
+    const url = "https://na.api.riotgames.com/val/content/v1/contents?locale=en-US&api_key=" + process.env.API_KEY
+    
+    const res = await fetch(url)
+    if (!res) {
+        throw new Error("Fetch request failed")
+    }
+    const { acts } = await res.json()
+    
+    return acts as Act[]
+
+}
